4af67e4e5a97aa90accf00e2121d6512241d6334
AkaElisee <akajeanelisee@gmail.com>
2020-04-24 11:51:33 +0200

    add function singup and netlify

---
+++ functions/signup.js
@@ -0,0 +1,10 @@
+// const fetch = require('node-fetch');
+// const dotenv = require('dotenv');
+
+// const mailchimpApi = process.env.GATSBY_API_KEY;
+
+// exports.handler = (event, context, callback) => {
+//     fetch(`Contact avec API `, {
+//         method:"POST"
+//     })
+// }
