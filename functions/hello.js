cc61bea7406df92b9f3d209425ea9ab06136c3ea
AkaElisee <akajeanelisee@gmail.com>
2020-04-24 12:32:52 +0200

    add function singup and netlify

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback){
-    console.log(event);
-    console.log(context);
+    // console.log(event);
+    // console.log(context);
     callback(null, {
         statusCode: 200,
         body: JSON.stringify("Hello !")
