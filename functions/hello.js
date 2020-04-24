c0168c6ee7fbe50c22c0853a6824c1a76856df64
AkaElisee <akajeanelisee@gmail.com>
2020-04-24 12:41:07 +0200

    hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback){
-    // console.log(event);
-    // console.log(context);
+    console.log(event);
+    console.log(event);
     callback(null, {
         statusCode: 200,
         body: JSON.stringify("Hello !")
