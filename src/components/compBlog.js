51228e330957f7d0cd809b74282e38988b33420a
AkaElisee <akajeanelisee@gmail.com>
2020-05-28 09:42:06 +0200

    add identity netlify

--- src/components/compBlog.js
+++ src/components/compBlog.js
@@ -25,23 +25,6 @@ const CompBlog = (props) => {
                             ))
                         }
                     </div>
-                    <div className="detail-article">
-                        {
-                            blog.map(({node}, index) => (
-                                <Card style= {{ width: "40rem"}} key={index}>
-                                    <Card.Img variant="top" src={node.frontmatter.image} />
-                                    <Card.Body>
-                                        <Card.Title> {node.frontmatter.title} </Card.Title>
-                                            <Card.Text>
-                                                {node.frontmatter.description}
-                                            </Card.Text>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <span>{node.frontmatter.created}</span>
-                                    </Card.Body>
-                                </Card>
-                            ))
-                        }
-                    </div>
                 </Col>
 
                 <Col md="4">
