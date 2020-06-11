70b4e46f5d881f6e7e6c64d38dd3bf7e18dc0bf0
AkaElisee <akajeanelisee@gmail.com>
2020-06-11 02:06:15 +0200

    Project final

--- src/components/ComProduitTemplate.js
+++ src/components/ComProduitTemplate.js
@@ -90,15 +90,6 @@ const ComProduitTemplate = (props) => {
           data-item-image={detail.frontmatter.image}
           data-item-url={"https://inspiring-visvesvaraya-f559ca.netlify.app/" + detail.frontmatter.path} 
       > Ajouter au panier </button> 
-      <button
-          className='snipcart-add-item'
-          data-item-id={detail.id}
-          data-item-price={detail.frontmatter.price}
-          data-item-name={detail.frontmatter.title}
-          data-item-description={detail.frontmatter.description}
-          data-item-image={detail.frontmatter.image}
-          data-item-url={"https://inspiring-visvesvaraya-f559ca.netlify.app/" + detail.frontmatter.path} 
-      > Ajouter au panier 1 </button> 
       )
     }
     else{
@@ -136,6 +127,15 @@ const ComProduitTemplate = (props) => {
                       data-item-image={detail.frontmatter.image}
                       data-item-url={"https://inspiring-visvesvaraya-f559ca.netlify.app/" + detail.frontmatter.path} 
                   > Ajouter au panier 2</button> 
+                  <button type="hidden"
+                      className='snipcart-add-item'
+                      data-item-id={detail.id}
+                      data-item-price={detail.frontmatter.price}
+                      data-item-name={detail.frontmatter.title}
+                      data-item-description={detail.frontmatter.description}
+                      data-item-image={detail.frontmatter.image}
+                      data-item-url={"https://inspiring-visvesvaraya-f559ca.netlify.app/" + detail.frontmatter.path} 
+                  > Ajouter au panier 3</button> 
                   </div>
                 </div>
             </div>
